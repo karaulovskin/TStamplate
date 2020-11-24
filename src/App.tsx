20a59cadefba2a4a684d56742871650f61905ef0
Karaulovskin <karaulovskin@yandex.ru>
2020-11-24 19:04:36 +0300

    add ESlint && Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from 'react';
 import cn from 'classnames';
 
 import s from './App.module.scss';
 import './custom.css';
 
 const App = () => {
-    return (
-        <div className={cn(s.header, 'red')}>
-            This is App
-        </div>
-    )
-}
+  console.log('###: Some log');
+
+  return <div className={cn(s.header, 'red')}>This is App</div>;
+};
 
 export default App;
